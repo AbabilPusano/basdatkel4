ff8378a6d3af6b553eada3d9bdce6f2e21f544fc
Ababil <ababilpuusano@gmail.com>
2024-06-03 16:02:55 +0700

    dinamis transaksi page

--- app/controllers/ManajemenTransaksi.php
+++ app/controllers/ManajemenTransaksi.php
@@ -4,6 +4,7 @@ class ManajemenTransaksi extends Controller{
     public function index()
     {
         $data['judul'] = 'Manajemen Transaksi';
+        $data['trx'] = $this->model('TransaksiModel')->getAllNama();
 
         $this->view('templates/header', $data);
         $this->view('manajementransaksi/index', $data);
